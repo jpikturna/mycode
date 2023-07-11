410290a0bf37f04fc3ec96271f6112a34b511ec6
jpikturna <jppikturna@gmail.com>
2023-07-11 21:18:50 +0000

    About complex lists

---
+++ advlist/animal-list.py
@@ -0,0 +1,11 @@
+#!/user/bin/env python3
+"""Alta3 Class Extra - Animal List"""
+
+def main():
+
+    animals = ["dog", "cat", "rat", "bat", "cow", "yak", "hog"]
+
+    print(" ".join(animals))
+
+if __name__ == "__main__":
+    main()
